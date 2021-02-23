5e9065b98c55ca2179ce069e10c39c095502d99c
marco <marco@192.168.0.103>
2021-02-23 20:38:14 -0300

    Habilitado para funcionar o PHP 7.4

--- PessoaView.php
+++ PessoaView.php
@@ -38,17 +38,17 @@
             <td style="padding-left: 10px">
                 <h2 style="text-align: center;"><?= $this->acaoDescricao ?> Pessoa</h2> 
                 <form method="POST">
-                    <input type="text" name="ID_PESSOA" value="<?= $this->dado['ID_PESSOA'] ?>" hidden>
+                    <input type="text" name="ID_PESSOA" value="<?= @$this->dado['ID_PESSOA'] ?>" hidden>
                     <label>Nome:</label>
-                    <input type="text" name="NOME" value="<?= $this->dado['NOME'] ?>" minlength="3" maxlength="100" required><br>
+                    <input type="text" name="NOME" value="<?= @$this->dado['NOME'] ?>" minlength="3" maxlength="100" required><br>
                     <label>UF:</label>
                     <select name="UF" required>
                         <option></option>
-                        <option value="SC" <?= ($this->dado['UF'] == 'SC' ? 'selected' : '') ?>>SC</option>
-                        <option value="OU" <?= ($this->dado['UF'] == 'OU' ? 'selected' : '') ?>>Outro</option>
+                        <option value="SC" <?= (@$this->dado['UF'] == 'SC' ? 'selected' : '') ?>>SC</option>
+                        <option value="OU" <?= (@$this->dado['UF'] == 'OU' ? 'selected' : '') ?>>Outro</option>
                     </select><br>
                     <label style="font-weight: normal">Observação:</label>
-                    <textarea maxlength="1000" style="height: 100px" name="OBSERVACAO"><?= $this->dado['OBSERVACAO'] ?></textarea>
+                    <textarea maxlength="1000" style="height: 100px" name="OBSERVACAO"><?= @$this->dado['OBSERVACAO'] ?></textarea>
                     <hr>
                     <div style="border: 0px solid; text-align: center">
                         <input name="ACAO" value="<?= $this->acaoDescricao ?>" type="submit">
